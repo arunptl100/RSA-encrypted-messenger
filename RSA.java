ed998b7f451486dc337a23bc2d59e54ddb2e829e
arunptl100 <arunptl100@hotmail.com>
2019-08-04 21:43:13 +0100

    removed duplicate functions

--- RSA.java
+++ RSA.java
@@ -13,30 +13,13 @@ public class RSA{
     return findGCD(number2, number1%number2);
   }
 
-  /* Function to determing if 2 numbers are relatively prime
+  /* Function to determine if 2 numbers are relatively prime
    * returns true if the gcd of the 2 numbers is 1 , false otherwise
    */
   public static boolean relativelyPrime(int a , int b){
     return (findGCD(a, b) == 1) ? true : false;
   }
 
-  public static int CongruenceModulo(int b , int n){
-    // returns a in a == b mod n
-    int k = 1;
-    int a = (k * n) + b;
-    while((a % n) != (b % n)){
-      k++;
-      a = (k * n) + b;
-      if(k == 10){return -1;} //threshold for k reached , quit
-    }
-    return a;
-
-    //congruence a == b mod n
-    // a = k*n + b
-    // in 4 mod 10
-    // a = k * 10 + 4 ... taking k = 2
-    // a = 24 ... 24 == 4 mod 10
-  }
   /*
   * p and q are large prime numbers
   * e must be relatively prime to (p-1) and (q-1) and (p-1)(q-1)
--- Sender.java
+++ Sender.java
@@ -22,21 +22,4 @@ public class Sender{
     return ((Message).pow(this.e_pubKey)).mod(BigInteger.valueOf(this.n_pubKey));
   }
 
-  public int CongruenceModulo(int b , int n){
-    // returns a in a == b mod n
-    int k = 1;
-    int a = (k * n) + b;
-    while((a % n) != (b % n)){
-      k++;
-      a = (k * n) + b;
-      if(k == 10){return -1;} //threshold for k reached , quit
-    }
-    return a;
-
-    //congruence a == b mod n
-    // a = k*n + b
-    // in 4 mod 10
-      // a = k * 10 + 4 ... taking k = 2
-      // a = 24 ... 24 == 4 mod 10
-  }
 }
