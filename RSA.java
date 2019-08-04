7bd63d134b3655e80b6e37668ef084ba6df6dcf7
arunptl100 <arunptl100@hotmail.com>
2019-08-04 17:45:42 +0100

    completed implementation

--- RSA.java
+++ RSA.java
@@ -2,10 +2,10 @@ import java.math.BigInteger;
 
 public class RSA{
 
-  public static BigInteger CongruenceModulo(double b , double n){
+  public static int CongruenceModulo(int b , int n){
     // returns a in a == b mod n
-    double k = 1;
-    double a = (k * n) + b;
+    int k = 1;
+    int a = (k * n) + b;
     while((a % n) != (b % n)){
       k++;
       a = (k * n) + b;
@@ -24,28 +24,30 @@ public class RSA{
     System.out.println("====RSA encryption implemtation====");
     // double x = CongruenceModulo(4,10);
     // System.out.println(x + " == " + " 4 mod 10" );
-    BigInteger n, p, q, e, d, Message, Ciphertext;
-    Message = new BigInteger("88");
-    p = new BigInteger("17");
-    q = new BigInteger("11");
-    n = p.multiply(q); // n pubic key = p * q
+    int n, p, q, e, d;
+    BigInteger Message, Ciphertext;
+    Message = new BigInteger("150");
+    p = 17;
+    q = 11;
+    n = p*q; // n pubic key = p * q
 
     System.out.println("Using public key " + n);
     // e must be relatively prime to (p-1) and (q-1) and (p-1)(q-1)
     // 1 < e < (p-1)(q-1)
-    e = new BigInteger("7");
+    e = 7;
     //private key d -> e * d == 1 mod ((p-1)(q-1))
-    d = (CongruenceModulo(1, ((p.subtract(new BigInteger("1"))).multiply(q.subtract(new BigInteger("1"))))))/e;
+    d = (CongruenceModulo(1, ((p-1)*(q-1))))/e;
 
     System.out.println("private key " + d);
     System.out.println("Encrypting Message " + Message + " with public key " + n);
     //Ciphertext == M^e mod n
-    Ciphertext = (new BigInteger(Message)).pow((int)e).mod(n);
+    //Ciphertext can be large so use BigInteger
+    Ciphertext = (Message).pow(e).mod(BigInteger.valueOf(n));
 
     //Ciphertext = (Math.pow(Message, e))%n;
     System.out.println("Ciphertext " + Ciphertext);
     System.out.println("Decrypting message "+ Ciphertext + "^" + d + "mod" + n);
-    Message = Ciphertext.pow((int)d) % n;
+    Message = Ciphertext.pow(d).mod(BigInteger.valueOf(n));
     System.out.println("Decrypted message "+ Message);
     //Decryption -> M == C^d mod n
 
